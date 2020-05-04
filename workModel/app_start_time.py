b3bc72515415de26237edb833b9ef4c38d02280d
Anne9292 <976413691@qq.com>
2020-05-04 21:09:44 +0800

    commit app_start_time.py update

--- workModel/app_start_time.py
+++ workModel/app_start_time.py
@@ -5,7 +5,7 @@ import os
 import csv
 import time
 
-# 测试app冷启动和热启动时间
+'''测试app冷启动和热启动时间'''
 class App(object):
 
     def __init__(self):
@@ -14,13 +14,13 @@ class App(object):
 
     # 启动APP
     def launch_app(self):
-        cmd = 'adb shell am start -W -n asia.bluepay.clientin/.business.module.login.FirstActivity '
-        self.content = os.popen(cmd)
+        cmd = 'adb shell am start -W -n asia.bluepay.clientin/.business.module.login.FirstActivity'
+        self.content = os.popen(cmd)  # 实时输出类文件对象，不会实时输出执行结果
 
     # 停止APP
     def stop_app(self):
-        cmd = 'adb shell am force-stop asia.bluepay.clientin'
-        # cmd = 'adb shell input keyevent 3'
+        cmd = 'adb shell am force-stop asia.bluepay.clientin'  # 冷启动关闭app
+        # cmd = 'adb shell input keyevent 3'  热启动关闭app
         os.popen(cmd)
 
     # 获取启动时间
@@ -30,6 +30,7 @@ class App(object):
                 self.total_time = line.split(': ')[1].split('\n')[0]
                 break
         return self.total_time
+
 
 # 控制类
 class Controller(object):
@@ -47,6 +48,7 @@ class Controller(object):
         time.sleep(3)
         current_time = self.get_current_time()
         self.all_data.append((current_time,elapsed_time))
+        print(self.all_data)
 
     # 多次执行测试过程
     def run(self):
@@ -67,6 +69,6 @@ class Controller(object):
 
 
 if __name__ == '__main__':
-    controller = Controller(1)
-    controller.run()
+    controller = Controller(5)
+    controller.test_process()
     controller.save_data_to_csv()
