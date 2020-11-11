8f73448aafbbe97f703b3a90943e7724ebb674c8
anne <976413691@qq.com>
2020-11-11 11:51:35 +0700

    '提交-job'

---
+++ InterviewDemo/compare.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import json
+
+def cmp(src_f1,dst_f2):
+
+    for src_value, dst_value in zip(sorted(src_f1), sorted(dst_f2)):
+
+        if str(src_f1[src_value]) != str(dst_f2[dst_value]):
+            print(src_value,src_f1[src_value],dst_value,dst_f2[dst_value])
+
+if __name__ == '__main__':
+    # src_data = {"name":"anne","age":20,"source":["math","chinese"],"grade":1,"pets":{"dog":"tangshao","age":[1, 2]}}
+    # dst_data = {"name":"anne","age":20,"source":["english","math","chinese"],"grade":1}
+    src_data = "src_data.json"
+    dst_data = "dst_data.json"
+    with open(src_data) as f1:
+        f1_dict = json.load(f1)
+    with open(dst_data) as f2:
+        f2_dict = json.load(f2)
+    cmp(f1_dict,f2_dict)
---
+++ InterviewDemo/sum.py
@@ -0,0 +1,13 @@
+def test_twoSum(nums, target):
+        dct = {}
+        for index, num in enumerate(nums):
+            another_num = target - nums[index]
+            if another_num in dct:   # 在dct字典中，another_num是key
+                return [nums.index(another_num), index]
+            else:
+                dct[num] = index
+
+if __name__ == '__main__':
+    list1 = [2,7,11,15]
+    result = test_twoSum(list1, 17)
+    print(result)
