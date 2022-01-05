6a83e767ac51b0d92766508f172bd53a102fd0e7
annecode <976413691@qq.com>
2022-01-05 16:33:19 +0800

    删除乱码文件

---
+++ ObjectDemo/绫诲疄渚嬪寲.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+
+# class MyClass(object):  # 新式类
+class MyClass:            # 经典类
+    """一个简单的类实例"""
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+
+
+if __name__ == '__main__':
+
+    # 实例化类
+    x = MyClass()
+    # 访问类的属性和方法
+    print("MyClass 类的属性i为：", x.i)
+    print("MyClass 类的方法f输出为：", x.f())
---
+++ ObjectDemo/缁ф壙.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author : anne yang
+
+class Animals(object):
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+
+    def eat(self):
+        name = 'cat'
+        category = 'roof'
+        print('真的是动物呀', name, category)
+
+if __name__ == '__main__':
+    dog = Animals('la', 'ee')
+    dog.eat()
+    print(dog.eat())
